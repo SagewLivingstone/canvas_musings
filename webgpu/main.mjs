dc8bf9000b07b4a55dde5859dff31674b8e8baed
SageWLivingstone <sage.livingstone@geovisual-analytics.com>
2023-11-29 16:35:44 +0900

    clearing canvas background

--- webgpu/main.mjs
+++ webgpu/main.mjs
@@ -1,20 +1,37 @@
 
 const canvas = document.querySelector('canvas');
 
+// Check if webgpu is supported
 if (!navigator.gpu) {
     throw new Error("WebGPU not supported");
 }
 
+// Check if we have a compatible device to grab
 const adapter = await navigator.gpu.requestAdapter();
 if (!adapter) {
     throw new Error("No appropriate GPU adapter found");
 }
 
+// Configure canvas to use device
 const device = await adapter.requestDevice();
 const context = canvas.getContext("webgpu");
 const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
 context.configure({
     device: device,
-    format: canvasFormat,
+    format: canvasFormat, // Use the texture format recommended
 });
 
+// Build a command encoder, and give it some commands
+const encoder = device.createCommandEncoder();
+const pass = encoder.beginRenderPass({
+    colorAttachments: [{
+        view: context.getCurrentTexture().createView(),
+        loadOp: "clear",
+        clearValue: [0, 0.35, 0.4, 1],
+        storeOp: "store",
+    }]
+});
+pass.end();
+
+// Submit the command buffer to the device's queue for processing
+device.queue.submit([encoder.finish()]);
