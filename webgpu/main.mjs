5c19b909de912b84aad4a4fbd7cb1fedef2b0ca3
SageWLivingstone <sage.livingstone@geovisual-analytics.com>
2023-11-30 01:26:59 -0800

    drawing a single cell

--- webgpu/main.mjs
+++ webgpu/main.mjs
@@ -21,6 +21,71 @@ context.configure({
     format: canvasFormat, // Use the texture format recommended
 });
 
+const vertices = new Float32Array([
+//   X,    Y,
+  -0.8, -0.8, // Triangle 1 
+   0.8, -0.8,
+   0.8,  0.8,
+
+  -0.8, -0.8, // Triangle 2 
+   0.8,  0.8,
+  -0.8,  0.8,
+]);
+// Create a buffer to hold the vertex data
+const vertexBuffer = device.createBuffer({
+    label: "Cell vertices",
+    size: vertices.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+
+const vertexBufferLayout = {
+    arrayStride: 8,
+    attributes: [{
+        format: "float32x2",
+        offset: 0,
+        shaderLocation: 0, // Vertex shader location
+    }]
+}
+
+const cellShaderModule = device.createShaderModule({
+    label: "Cell Shader",
+    code: `
+        @vertex
+        fn vertexMain(@location(0) pos: vec2f)
+            -> @builtin(position) vec4f
+        {
+            return vec4f(pos, 0.0, 1.0);
+        }
+
+        
+        @fragment
+        fn fragmentMain() -> @location(0) vec4f
+        {
+            return vec4f(1, 0, 0, 1);
+        }
+    `
+});
+
+// Build render pipeline
+const cellPipeline = device.createRenderPipeline({
+    label: "Cell Pipeline",
+    layout: "auto",
+    vertex: {
+        module: cellShaderModule,
+        entryPoint: "vertexMain",
+        buffers: [vertexBufferLayout],
+    },
+    fragment: {
+        module: cellShaderModule,
+        entryPoint: "fragmentMain",
+        targets: [{
+            format: canvasFormat,
+        }],
+    }
+})
+
+
 // Build a command encoder, and give it some commands
 const encoder = device.createCommandEncoder();
 const pass = encoder.beginRenderPass({
@@ -31,6 +96,11 @@ const pass = encoder.beginRenderPass({
         storeOp: "store",
     }]
 });
+
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+pass.draw(vertices.length / 2);
+
 pass.end();
 
 // Submit the command buffer to the device's queue for processing
